0e83f41d24bdd5f188542fd16793773256e38ffb
Kunal Sonawane <scorpionboy30@gmail.com>
2014-12-07 15:30:10 -0800

    category db updated

--- CategoryDB.py
+++ CategoryDB.py
@@ -1,6 +1,7 @@
 import pymysql
 import json
 import DBConnectionPool
+from bottle import error,response
 
 def cust_error(statuscode,message):
 	error = dict();
@@ -16,12 +17,17 @@ def getAllCategories():
 		cursor = DBConnectionPool.dbconnect()
 		cursor.execute("Select categoryId, categoryName from Category")
 		row = cursor.fetchone()
-		d = dict()
-		d['categoryId'] = row[0]
-		d['categoryName'] = row[1]
-		response = json.dumps(d, indent = 4)
-		cursor.close()
-		return response
+		if row is not None:
+			d = dict()
+			d['categoryId'] = row[0]
+			d['categoryName'] = row[1]
+			response = json.dumps(d, indent = 4)
+			cursor.close()
+			return response
+		else:
+			errorResponse = cust_error(404,"Category not found. Please create a new category.")
+			cursor.close()
+			return errorResponse
 	except:
 		errorResponse = cust_error(500,"Something went wrong in processing at server side")
 		cursor.close()
@@ -29,14 +35,20 @@ def getAllCategories():
 
 def getCategoryById(categoryId):
 	try:
+		cursor = DBConnectionPool.dbconnect()
 		cursor.execute("Select categoryId, categoryName from Category where categoryId=%s",categoryId)
 		row = cursor.fetchone()
-		d = dict()
-		d['categoryId'] = row[0]
-		d['categoryName'] = row[1]
-		response = json.dumps(d, indent = 4)
-		cursor.close()
-		return response
+		if row is not None:
+			d = dict()
+			d['categoryId'] = row[0]
+			d['categoryName'] = row[1]
+			response = json.dumps(d, indent = 4)
+			cursor.close()
+			return response
+		else:
+			errorResponse = cust_error(404,"Category not found. Please check your categoryid again.")
+			cursor.close()
+			return errorResponse
 	except:
 		errorResponse = cust_error(500,"Something went wrong in processing at server side")
 		cursor.close()
@@ -44,15 +56,21 @@ def getCategoryById(categoryId):
 	
 def createCategoryByName(categoryName):
 	try:
+		cursor=DBConnectionPool.dbconnect()
 		cursor.execute("insert into Category (categoryName) values(%s)",categoryName)	
 		cursor.execute("Select * from Category where categoryName=%s",categoryName)
 		row = cursor.fetchone()
-		d = dict()
-		d['categoryId'] = row[0]
-		d['categoryName'] = row[1]
-		response = json.dumps(d, indent = 4)
-		cursor.close()
-		return response
+		if row is not None:
+			d = dict()
+			d['categoryId'] = row[0]
+			d['categoryName'] = row[1]
+			response = json.dumps(d, indent = 4)
+			cursor.close()
+			return response
+		else:
+			errorResponse = cust_error(404,"Category not created properly.")
+			cursor.close()
+			return errorResponse
 	except:
 		errorResponse = cust_error(500,"Something went wrong in processing at server side")
 		cursor.close()
